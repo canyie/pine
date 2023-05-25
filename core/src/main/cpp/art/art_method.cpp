canyie/pine

commit 4777c2c542e4a0de171eef5f3eaa3856f19f7afd

[core] Fix null pointer dereference on Android 6.0

core/src/main/cpp/art/art_method.cpp
@@ -28,7 +28,7 @@ Member<ArtMethod, uint32_t> ArtMethod::access_flags_;
 Member<ArtMethod, void*> ArtMethod::entry_point_from_jni_;
 Member<ArtMethod, void*> ArtMethod::entry_point_from_compiled_code_;
 Member<ArtMethod, void*>* ArtMethod::entry_point_from_interpreter_;
-Member<ArtMethod, uint32_t>* ArtMethod::declaring_class = nullptr;
+Member<ArtMethod, uint32_t> ArtMethod::declaring_class;
 
 void ArtMethod::Init(const ElfImg* handle) {
     art_quick_to_interpreter_bridge = handle->GetSymbolAddress("art_quick_to_interpreter_bridge");
@@ -184,13 +184,9 @@ void ArtMethod::InitMembers(JNIEnv* env, ArtMethod* m1, ArtMethod* m2, ArtMethod
         }
 
         if (Android::version < Android::kN) {
-            // Not align: PtrSizedFields is PACKED(4) in the android version.
+            // Not aligned: PtrSizedFields is PACKED(4) in the android version.
             entry_point_from_interpreter_ = new Member<ArtMethod, void*>(
                     entry_point_from_jni_.GetOffset() - entry_point_member_size);
-        } else {
-            // On Android 7.0+, the declaring_class may be moved by the GC,
-            // so we check and update it when invoke backup method.
-            declaring_class = new Member<ArtMethod, uint32_t>(0);
         }
 
 #if __ANDROID_API__ < __ANDROID_API_L__

core/src/main/cpp/art/art_method.h
@@ -57,14 +57,12 @@ namespace pine::art {
             return reinterpret_cast<jmethodID>(this);
         }
 
-        // Only works on android 7.0+
         uint32_t GetDeclaringClass() {
-            return declaring_class->Get(this);
+            return declaring_class.Get(this);
         }
 
-        // Only works on android 7.0+
         void SetDeclaringClass(uint32_t new_declaring_class) {
-            declaring_class->Set(this, new_declaring_class);
+            declaring_class.Set(this, new_declaring_class);
         }
 
         bool IsCompiled() {
@@ -339,7 +337,7 @@ namespace pine::art {
         static Member<ArtMethod, void*> entry_point_from_jni_;
 
         static Member<ArtMethod, void*>* entry_point_from_interpreter_;
-        static Member<ArtMethod, uint32_t>* declaring_class; // GCRoot is uint32_t
+        static Member<ArtMethod, uint32_t> declaring_class; // GCRoot is uint32_t
 
         DISALLOW_IMPLICIT_CONSTRUCTORS(ArtMethod);
     };

commit 24e61b3520e887922f306e4afed1e89ff394d38b

[core] Fix a use-before-initialization regression introduced in 4777c2c5
When you was tired, just go to sleep. You can break everything by working when tired...

core/src/main/cpp/art/art_method.cpp
@@ -111,6 +111,9 @@ void ArtMethod::InitMembers(JNIEnv* env, ArtMethod* m1, ArtMethod* m2, ArtMethod
 
     size = Difference(reinterpret_cast<intptr_t>(m1), reinterpret_cast<intptr_t>(m2));
     int android_version = Android::version;
+
+    if (android_version >= Android::kM)
+        declaring_class.SetOffset(0);
 #if __ANDROID_API__ < __ANDROID_API_L__
     if (LIKELY(android_version >= Android::kL)) {
 #endif

core/src/main/cpp/art/art_method.h
@@ -57,10 +57,12 @@ namespace pine::art {
             return reinterpret_cast<jmethodID>(this);
         }
 
+        // Only works on Android 6.0+
         uint32_t GetDeclaringClass() {
             return declaring_class.Get(this);
         }
 
+        // Only works on Android 6.0+
         void SetDeclaringClass(uint32_t new_declaring_class) {
             declaring_class.Set(this, new_declaring_class);
         }