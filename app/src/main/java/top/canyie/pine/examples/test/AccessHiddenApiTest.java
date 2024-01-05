13605c4542b9bfadfe6ac67aa6c31e4750b0638d
canyie <a1364259@163.com>
2024-01-05 15:05:15 +0800

    [app] Fix test failed on Android < 9.0

--- app/src/main/java/top/canyie/pine/examples/test/AccessHiddenApiTest.java
+++ app/src/main/java/top/canyie/pine/examples/test/AccessHiddenApiTest.java
@@ -1,5 +1,6 @@
 package top.canyie.pine.examples.test;
 
+import android.os.Build;
 import android.util.Log;
 
 import top.canyie.pine.examples.ExampleApp;
@@ -11,10 +12,12 @@ public class AccessHiddenApiTest extends Test {
     @Override public int run() {
         try {
             isCallbackInvoked = true;
-            Class<?> VMRuntime = Class.forName("dalvik.system.VMRuntime");
-            VMRuntime.getDeclaredMethod("setHiddenApiExemptions", String[].class)
-                    .invoke(VMRuntime.getDeclaredMethod("getRuntime").invoke(null),
-                            (Object) new String[] { "" });
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                Class<?> VMRuntime = Class.forName("dalvik.system.VMRuntime");
+                VMRuntime.getDeclaredMethod("setHiddenApiExemptions", String[].class)
+                        .invoke(VMRuntime.getDeclaredMethod("getRuntime").invoke(null),
+                                (Object) new String[] {""});
+            }
             return SUCCESS;
         } catch (ReflectiveOperationException e) {
             Log.e(ExampleApp.TAG, "Access Hidden API test failed", e);
